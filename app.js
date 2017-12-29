b674a57bc2e2a0f0f4ca2236312f70f68dd73fa0
Vixar Vorlavongc <vixar339@gmail.com>
2017-12-29 16:44:07 +0700

    upload first project

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/app/views/index.html');
+});
+
+server.listen(PORT, () => {
+    console.log('server is running');
+});
